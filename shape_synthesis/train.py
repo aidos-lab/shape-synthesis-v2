79823b02398ef856e22409458f677e287cfa2889
Ernst Röell <ernstroell@gmail.com>
2025-05-23 14:50:57 +0200

    Baseline

--- shape_synthesis/train.py
+++ shape_synthesis/train.py
@@ -12,13 +12,14 @@ from shape_synthesis.datasets.mnist import DataConfig, get_dataloaders
 from shape_synthesis.datasets.transforms import EctTransform
 from shape_synthesis.models.vae import VAE
 
-IMG_SIZE = 128
-LATENT_DIM = 400
-HIDDEN_DIM = 800
+IMG_SIZE = 28
+LATENT_DIM = 200
+HIDDEN_DIM = 400
 
 
 def loss_function(x, x_hat, mean, log_var):
-    reproduction_loss = nn.functional.binary_cross_entropy(x_hat, x, reduction="sum")
+    # reproduction_loss = nn.functional.binary_cross_entropy(x_hat, x, reduction="sum")
+    reproduction_loss = nn.functional.mse_loss(x_hat, x, reduction="sum")
     KLD = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
 
     return reproduction_loss + KLD
